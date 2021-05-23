028c66015042dca620b7d09a10ae0860eb85b27d
Phillip Kluge <52058352+phillipkluge@users.noreply.github.com>
2021-05-22 20:51:19 -0600

    Add files via upload
    Software to control the stepper motor, can currently change the degrees of rotation through the code.

---
+++ stepper_motor.ino
@@ -0,0 +1,20 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 360;
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(64);
+  // initialize the serial port:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // step one revolution  in one direction:
+  Serial.println("clockwise");
+  myStepper.step(2048);
+  delay(5000);
+}
